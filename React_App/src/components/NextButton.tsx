249559c6cd1a3079791ae606d624a0c03f29d25a
GabrielAlfaro07 <galfaulate@gmail.com>
2024-08-21 22:37:28 -0600

    Modularized previous and next buttons

--- React_App/src/components/NextButton.tsx
+++ React_App/src/components/NextButton.tsx
@@ -10,7 +10,7 @@ const NextButton: React.FC<NextButtonProps> = ({ onClick, disabled }) => {
     <button
       onClick={onClick}
       disabled={disabled}
-      className="px-4 py-2 bg-gray-600 text-white rounded-full flex items-center justify-center"
+      className="px-4 py-2 bg-gray-600 text-white rounded-full flex items-center justify-center transition duration-300 ease-in-out hover:bg-gray-500 disabled:bg-gray-400"
       style={{ width: "100px", height: "40px" }} // Fixed size for the button
     >
       Next
--- React_App/src/components/PreviousButton.tsx
+++ React_App/src/components/PreviousButton.tsx
@@ -13,7 +13,7 @@ const PreviousButton: React.FC<PreviousButtonProps> = ({
     <button
       onClick={onClick}
       disabled={disabled}
-      className="px-4 py-2 bg-gray-600 text-white rounded-full flex items-center justify-center"
+      className="px-4 py-2 bg-gray-600 text-white rounded-full flex items-center justify-center transition duration-300 ease-in-out hover:bg-gray-500 disabled:bg-gray-400"
       style={{ width: "100px", height: "40px" }} // Fixed size for the button
     >
       Previous
